b5a9130a1fdcb27ab7846de439e4ed1a65bf6305
Kabe-H <75508588+Kabe-H@users.noreply.github.com>
2023-05-12 08:27:16 -0300

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <div id="horizontalLine">
+        <div id="color1"></div>
+        <div id="color2"></div>
+        <div id="color3"></div>
+        <div id="color4"></div>
+        <div id="color5"></div>
+        <div id="color6"></div>
+        <div id="color7"></div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
